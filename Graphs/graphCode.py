9f37c3e3ee7c17c20cfbdd10e9e8a4e2ed10937d
preethamgouda <72795407+preethamgouda@users.noreply.github.com>
2023-11-14 19:51:09 +0530

    Update graphCode.py

--- Graphs/graphCode.py
+++ Graphs/graphCode.py
@@ -2,130 +2,115 @@ import sys
 
 class Graph:
 
-	# Class to represent individual Vertex
-	# Design consists of vertexID and a
-	# dictionary to store neighbours info of that vertex
-	class _Vertex:
-		def __init__(self, vID):
-			self.id = vID
-			self.adjacentVertex = {}
+    class Vertex:
+        def __init__(self, vertex_id):
+            self.id = vertex_id
+            self.adjacent_vertices = {}
 
-		def _isNeighbour(self, vertex):
-			return vertex in self.adjacentVertex
+        def is_neighbour(self, vertex):
+            return vertex in self.adjacent_vertices
 
-		def _addNeighbour(self, id, wt = 0):
-			if not self._isNeighbour(id):
-				self.adjacentVertex[id] = wt
+        def add_neighbour(self, vertex_id, weight=0):
+            if not self.is_neighbour(vertex_id):
+                self.adjacent_vertices[vertex_id] = weight
 
-		def _listNeighbours(self):
-			return self.adjacentVertex.keys()
+        def list_neighbours(self):
+            return list(self.adjacent_vertices.keys())
 
-		def _edgeCost(self, id):
-			if self._isNeighbour(id):
-				return self.adjacentVertex[id]
-			else:
-				return sys.maxsize
+        def edge_cost(self, vertex_id):
+            return self.adjacent_vertices.get(vertex_id, sys.maxsize)
 
-	def __init__(self):
-		self.vertex_count = 0
-		self.adjacency_list = {}
+    def __init__(self):
+        self.vertex_count = 0
+        self.adjacency_list = {}
 
-	def getVertexCount(self):
-		return self.vertex_count
+    def get_vertex_count(self):
+        return self.vertex_count
 
-	def addVertex(self, vid):
-		if not vid in self.adjacency_list:
-			self.vertex_count += 1
-			new_vertex = self._Vertex(vid)
-			self.adjacency_list[vid] = new_vertex
+    def add_vertex(self, vertex_id):
+        if vertex_id not in self.adjacency_list:
+            self.vertex_count += 1
+            new_vertex = self.Vertex(vertex_id)
+            self.adjacency_list[vertex_id] = new_vertex
 
-	def addEdge(self, frm, to, wt = 0):
-		if not frm in self.adjacency_list:
-			self.addVertex(frm)
-		if not to in self.adjacency_list:
-			self.addVertex(to)
+    def add_edge(self, from_vertex, to_vertex, weight=0):
+        if from_vertex not in self.adjacency_list:
+            self.add_vertex(from_vertex)
+        if to_vertex not in self.adjacency_list:
+            self.add_vertex(to_vertex)
 
-		self.adjacency_list[frm]._addNeighbour(to, wt)
-		self.adjacency_list[to]._addNeighbour(frm, wt)	# Not for Directed graph
+        self.adjacency_list[from_vertex].add_neighbour(to_vertex, weight)
+        self.adjacency_list[to_vertex].add_neighbour(from_vertex, weight)
 
-	def getEdgeCost(self, frm, to):
-		if frm in self.adjacency_list and to in self.adjacency_list:
-			return self.adjacency_list[frm]._edgeCost(to)
+    def get_edge_cost(self, from_vertex, to_vertex):
+        if from_vertex in self.adjacency_list and to_vertex in self.adjacency_list:
+            return self.adjacency_list[from_vertex].edge_cost(to_vertex)
 
-	def getVertexList(self):
-		if self.vertex_count != 0:
-			return (list(self.adjacency_list.keys()))
+    def get_vertex_list(self):
+        if self.vertex_count != 0:
+            return list(self.adjacency_list.keys())
 
-	def getNeighbourList(self, vid):
-		return (list(self.adjacency_list[vid]._listNeighbours()))
+    def get_neighbour_list(self, vertex_id):
+        return self.adjacency_list[vertex_id].list_neighbours()
 
-	# Returns only first discoverd path
-	def findPath(self, start, end, path=[]):
-		path = path + [start]
-		if start == end:
-			return path
-		if not start in self.getVertexList():
-			return None
-		for node in self.getNeighbourList(start):
-			if not node in path:
-				new_path = self.findPath(node, end, path)
-				if new_path:
-					return new_path
-		return None
+    def find_path(self, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return path
+        if start not in self.get_vertex_list():
+            return None
+        for node in self.get_neighbour_list(start):
+            if node not in path:
+                new_path = self.find_path(node, end, path)
+                if new_path:
+                    return new_path
+        return None
 
-	# Returns all existing path (not in a required format)
-	# Use displayPaths() to get paths in a proper way
-	def findAllPaths(self, start, end, path=[]):
-		path = path + [start]
-		if start == end:
-			return path
-		if not start in self.getVertexList():
-			return None
-		paths = []
-		for node in self.getNeighbourList(start):
-			if not node in path:
-				new_path = self.findAllPaths(node, end, path)
-				for new_way in new_path:
-					paths.append(new_way)
-		
-		return paths
+    def find_all_paths(self, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return path
+        if start not in self.get_vertex_list():
+            return None
+        paths = []
+        for node in self.get_neighbour_list(start):
+            if node not in path:
+                new_path = self.find_all_paths(node, end, path)
+                for new_way in new_path:
+                    paths.append(new_way)
+        
+        return paths
 
-	def displayPaths(self, paths):
-		indicies = [ i for i in range(len(paths)) if paths[i] == paths[0]]		
-		routes = []
-		for idx in range(len(indicies)):
-			if indicies[idx] != indicies[-1]: # to exclude the last value in list indicies
-				routes.append(paths[indicies[idx]:indicies[idx+1]])
-	
-		routes.append(paths[indicies[-1]:]) # last value from indicies is added outside loop
-		
-		return routes
+    def display_paths(self, paths):
+        indices = [i for i in range(len(paths)) if paths[i] == paths[0]]        
+        routes = []
+        for idx in range(len(indices) - 1):
+            routes.append(paths[indices[idx]:indices[idx + 1]])
+        
+        routes.append(paths[indices[-1]:])
+        return routes
 
 
-def testemptyGraph():
-	g1 = Graph()
-	assert (g1.getVertexCount() == 0)
+def test_empty_graph():
+    g1 = Graph()
+    assert g1.get_vertex_count() == 0
 
-def testGraph():
-	g1 = Graph()
-	g1.addVertex('A')
-	g1.addVertex('C')
-	g1.addVertex('B')	
-	g1.addVertex('D')
-	g1.addVertex('E')
-	
-	g1.addEdge('A', 'C', 20)	
-	g1.addEdge('A', 'B', 10)
-	g1.addEdge('A', 'D', 20)	
-	g1.addEdge('C', 'B', 20)
-	g1.addEdge('C', 'E', 20)
+def test_graph():
+    g1 = Graph()
+    g1.add_vertex('A')
+    g1.add_vertex('C')
+    g1.add_vertex('B')    
+    g1.add_vertex('D')
+    g1.add_vertex('E')
+    
+    g1.add_edge('A', 'C', 20)    
+    g1.add_edge('A', 'B', 10)
+    g1.add_edge('A', 'D', 20)    
+    g1.add_edge('C', 'B', 20)
+    g1.add_edge('C', 'E', 20)
 
-	#assert (g1.getVertexCount() == 3)
-	#assert (g1.getEdgeCost(1,2) == 10)
-	#print(g1.getNeighbourList(2))
-	#print(g1.getVertexList())
-	paths = g1.findAllPaths('E', 'D')
-	print(g1.displayPaths(paths))
+    paths = g1.find_all_paths('E', 'D')
+    print(g1.display_paths(paths))
 
-testemptyGraph()
-testGraph()
+test_empty_graph()
+test_graph()
